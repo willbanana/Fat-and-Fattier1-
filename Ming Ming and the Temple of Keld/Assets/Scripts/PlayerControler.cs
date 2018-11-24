5f8fbb0519ef9a13a507caac788fa0c37ffeee38
FiaMmm <monamaria33@yahoo.com>
2018-11-24 16:57:52 +0200

    movement ok

--- Ming Ming and the Temple of Keld/Assets/Scripts/PlayerControler.cs
+++ Ming Ming and the Temple of Keld/Assets/Scripts/PlayerControler.cs
@@ -5,19 +5,19 @@ using UnityEngine;
 public class PlayerControler : MonoBehaviour
 {
     [SerializeField]
-    private float movementSpeed, ok=0;
+    public float movementSpeed;
 
-    private bool facingRight;
+    //private bool facingRight;
 
 
     private Rigidbody2D rb;
     private Vector2 moveVelocity;
-    private Animator myAnimator;
+    public Animator myAnimator;
 
 
     void Start()
     {
-        
+
         rb = GetComponent<Rigidbody2D>();
         myAnimator = GetComponent<Animator>();
     }
@@ -28,63 +28,48 @@ public class PlayerControler : MonoBehaviour
         myAnimator.SetBool("upPressed", false);
         myAnimator.SetBool("downPressed", false);
         myAnimator.SetBool("rightPressed", false);
-        ok = 0;
+        
     }
-
     void Update()
     {
-        //left and right movement:
+        float moveHorizontal = 0f;
+        float moveVertical = 0f;
+         moveHorizontal = Input.GetAxis("Horizontal");
+         moveVertical = Input.GetAxis("Vertical");
 
-        if (Input.GetKey(KeyCode.A))
+        Vector2 moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        moveVelocity = moveInput.normalized * movementSpeed;
+
+        if (moveHorizontal < 0)
         {
             ResetPressed();
             myAnimator.SetBool("leftPressed", true);
-            ok = 1;
-            if (ok == 1)
-            {
-                rb.velocity = new Vector2(Input.GetAxis("Horizontal") * movementSpeed, rb.velocity.y);
-                ok = 0;
-            }
         }
-
-        if (Input.GetKey(KeyCode.D))
+        if (moveHorizontal>  0)
         {
             ResetPressed();
             myAnimator.SetBool("rightPressed", true);
-            ok = 2;
-            if (ok == 2)
-            {
-                rb.velocity = new Vector2(Input.GetAxis("Horizontal") * movementSpeed, rb.velocity.y);
-                ok = 0;
-            }
         }
 
-        //up and down movement:
-
-        if (Input.GetKey(KeyCode.W))
-        {
-            ResetPressed();
-            myAnimator.SetBool("upPressed", true);
-            ok = 3;
-            if (ok == 3)
-            {
-                rb.AddForce(transform.up * movementSpeed);
-                ok = 0;
-            }
-        }
-
-        if (Input.GetKey(KeyCode.S))
+        if (moveVertical < 0)
         {
             ResetPressed();
             myAnimator.SetBool("downPressed", true);
-            ok = 4;
-            {
-                rb.AddForce(transform.up * -movementSpeed);
-                ok = 0;
-            }
-            
         }
+
+        if (moveVertical > 0)
+        {
+            ResetPressed();
+            myAnimator.SetBool("upPressed", true);
+        }
+
     }
-        
-        
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + moveVelocity * Time.fixedDeltaTime);
+
     }
+    
+
+}
